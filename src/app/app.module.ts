96e8b94d709a8f0b589cfb4dccbb876412bd5281
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-10 08:09:49 -0500

    Created a form component for the tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { TaskItemComponent } from './component/task-item/task-item.component';
+import { TaskFormComponent } from './component/task-form/task-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TaskItemComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
